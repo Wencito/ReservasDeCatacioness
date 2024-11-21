c2201885a9b74ec2d49feaae58bf7d9bf0d0cb50
Weencito <felileal25@gmail.com>
2024-11-21 08:05:29 -0300

    oki

--- Html/Español html/Dashboard/backend/index.js
+++
@@ -1,7 +0,0 @@
-const http = require('node:http'); //Node va a requerir un paquete en este caso http
-const requestHandler = require('./request-handler');
-const server = http.createServer(requestHandler);
-
-server.listen(4000, ()=>{
-    console.log('El servidor esta escuchando peticiones en http://localhost:4000/')
-});
